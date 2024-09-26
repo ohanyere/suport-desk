f1fe4b47fb39364ea038796f38ab828842c32ee3
Ohanyere Francis <mooref068@gmail.com>
2024-09-26 03:10:28 -0700

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log("server...");
+const express = require("express")
+const dotenv = require("dotenv").config()
+
+const app = express()
+
+const port =  process.env.PORT || 5000
+app.get("/", (reg, res) => {
+    res.status(200).json({"message":"Little drops of water everyday, makes a mighty ocean"})
+})
+
+app.listen(port, () => console.log(`server started at port ${port}`))
